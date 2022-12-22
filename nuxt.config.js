6478f68a9ce36399fe66aeb3adee40d2f1eba74e
Abdulaziz Almohsen <abdulaziz@exeque.co>
2022-12-22 19:12:38 +0300

    fb verification code

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,13 +9,14 @@ export default {
   head: {
     title: 'broastedkey',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      {name: 'acebook-domain-verification', content :'ojxevduabfzalf3uf3kijjt1mufmed'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
--- pages/index.vue
+++ pages/index.vue
@@ -1,226 +1,223 @@
+
+
 <template>
   <!-- component -->
-  <head>
-    <meta
-      name="facebook-domain-verification"
-      content="ojxevduabfzalf3uf3kijjt1mufmed"
-    />
-    <div>
-      <div class="relative w-full">
-        <nav class="fixed z-10 w-full bg-white md:absolute md:bg-transparent">
-          <div class="container m-auto px-2 md:px-12 lg:px-7">
+  
+  <div>
+    <div class="relative w-full">
+      <nav class="fixed z-10 w-full bg-white md:absolute md:bg-transparent">
+        <div class="container m-auto px-2 md:px-12 lg:px-7">
+          <div
+            class="flex flex-wrap items-center justify-between py-3 gap-6 md:py-4 md:gap-0"
+          >
+            <div class="w-full px-6 flex justify-between lg:w-max md:px-0">
+              <a
+                href="https://tailus.io/blocks/hero-section"
+                aria-label="logo"
+                class="flex space-x-2 items-center"
+              >
+                <img
+                  src="https://tailus.io/sources/blocks/food-delivery/preview/images/icon.png"
+                  class="w-12"
+                  alt="tailus logo"
+                  width="144"
+                  height="133"
+                />
+                <span class="text-2xl font-bold text-yellow-900"
+                  >Broasted <span class="text-yellow-700">Key</span></span
+                >
+              </a>
+              <button
+                aria-label="humburger"
+                id="hamburger"
+                class="relative w-10 h-10 -mr-2 lg:hidden"
+              >
+                <div
+                  aria-hidden="true"
+                  id="line"
+                  class="inset-0 w-6 h-0.5 m-auto rounded bg-yellow-900 transtion duration-300"
+                ></div>
+                <div
+                  aria-hidden="true"
+                  id="line2"
+                  class="inset-0 w-6 h-0.5 mt-2 m-auto rounded bg-yellow-900 transtion duration-300"
+                ></div>
+              </button>
+            </div>
             <div
-              class="flex flex-wrap items-center justify-between py-3 gap-6 md:py-4 md:gap-0"
+              class="hidden w-full lg:flex flex-wrap justify-end items-center space-y-6 p-6 rounded-xl bg-white md:space-y-0 md:p-0 md:flex-nowrap md:bg-transparent lg:w-7/12"
             >
-              <div class="w-full px-6 flex justify-between lg:w-max md:px-0">
-                <a
-                  href="https://tailus.io/blocks/hero-section"
-                  aria-label="logo"
-                  class="flex space-x-2 items-center"
+              <div class="text-gray-600 lg:pr-4">
+                <ul
+                  class="space-y-6 tracking-wide font-medium text-sm md:flex md:space-y-0"
                 >
-                  <img
-                    src="https://tailus.io/sources/blocks/food-delivery/preview/images/icon.png"
-                    class="w-12"
-                    alt="tailus logo"
-                    width="144"
-                    height="133"
-                  />
-                  <span class="text-2xl font-bold text-yellow-900"
-                    >Broasted <span class="text-yellow-700">Key</span></span
-                  >
-                </a>
-                <button
-                  aria-label="humburger"
-                  id="hamburger"
-                  class="relative w-10 h-10 -mr-2 lg:hidden"
-                >
-                  <div
-                    aria-hidden="true"
-                    id="line"
-                    class="inset-0 w-6 h-0.5 m-auto rounded bg-yellow-900 transtion duration-300"
-                  ></div>
-                  <div
-                    aria-hidden="true"
-                    id="line2"
-                    class="inset-0 w-6 h-0.5 mt-2 m-auto rounded bg-yellow-900 transtion duration-300"
-                  ></div>
-                </button>
+                  <li>
+                    <a
+                      href="#"
+                      class="block md:px-4 transition hover:text-yellow-700"
+                    >
+                      <span>I've a restaurant</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block md:px-4 transition hover:text-yellow-700"
+                    >
+                      <span>Wishlist</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block md:px-4 transition hover:text-yellow-700"
+                    >
+                      <span>Cart</span>
+                    </a>
+                  </li>
+                </ul>
               </div>
               <div
-                class="hidden w-full lg:flex flex-wrap justify-end items-center space-y-6 p-6 rounded-xl bg-white md:space-y-0 md:p-0 md:flex-nowrap md:bg-transparent lg:w-7/12"
+                class="w-full space-y-2 border-yellow-200 lg:space-y-0 md:w-max lg:border-l"
               >
-                <div class="text-gray-600 lg:pr-4">
-                  <ul
-                    class="space-y-6 tracking-wide font-medium text-sm md:flex md:space-y-0"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        class="block md:px-4 transition hover:text-yellow-700"
-                      >
-                        <span>I've a restaurant</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block md:px-4 transition hover:text-yellow-700"
-                      >
-                        <span>Wishlist</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block md:px-4 transition hover:text-yellow-700"
-                      >
-                        <span>Cart</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div
-                  class="w-full space-y-2 border-yellow-200 lg:space-y-0 md:w-max lg:border-l"
+                <button
+                  type="button"
+                  title="Start buying"
+                  class="w-full py-3 px-6 text-center rounded-full transition active:bg-yellow-200 focus:bg-yellow-100 sm:w-max"
                 >
-                  <button
-                    type="button"
-                    title="Start buying"
-                    class="w-full py-3 px-6 text-center rounded-full transition active:bg-yellow-200 focus:bg-yellow-100 sm:w-max"
-                  >
-                    <span class="block text-yellow-800 font-semibold text-sm">
-                      Sign up
-                    </span>
-                  </button>
-                  <button
-                    type="button"
-                    title="Start buying"
-                    class="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max"
-                  >
-                    <span class="block text-yellow-900 font-semibold text-sm">
-                      Login
-                    </span>
-                  </button>
-                </div>
+                  <span class="block text-yellow-800 font-semibold text-sm">
+                    Sign up
+                  </span>
+                </button>
+                <button
+                  type="button"
+                  title="Start buying"
+                  class="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max"
+                >
+                  <span class="block text-yellow-900 font-semibold text-sm">
+                    Login
+                  </span>
+                </button>
               </div>
             </div>
           </div>
-        </nav>
-        <div class="relative bg-yellow-50">
-          <div
-            class="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7"
-          >
-            <div class="flex items-center flex-wrap px-2 md:px-0">
-              <div class="relative lg:w-6/12 lg:py-24 xl:py-32">
-                <h1
-                  class="font-bold text-4xl text-yellow-900 md:text-5xl lg:w-10/12"
+        </div>
+      </nav>
+      <div class="relative bg-yellow-50">
+        <div
+          class="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7"
+        >
+          <div class="flex items-center flex-wrap px-2 md:px-0">
+            <div class="relative lg:w-6/12 lg:py-24 xl:py-32">
+              <h1
+                class="font-bold text-4xl text-yellow-900 md:text-5xl lg:w-10/12"
+              >
+                Your favorite dishes, right at your door
+              </h1>
+              <form action="" class="w-full mt-12">
+                <div
+                  class="relative flex p-1 rounded-full bg-white border border-yellow-200 shadow-md md:p-2"
                 >
-                  Your favorite dishes, right at your door
-                </h1>
-                <form action="" class="w-full mt-12">
-                  <div
-                    class="relative flex p-1 rounded-full bg-white border border-yellow-200 shadow-md md:p-2"
+                  <select
+                    class="hidden p-3 rounded-full bg-transparent md:block md:p-4"
+                    name="domain"
+                    id="domain"
                   >
-                    <select
-                      class="hidden p-3 rounded-full bg-transparent md:block md:p-4"
-                      name="domain"
-                      id="domain"
+                    <option value="design">FastFood</option>
+                    <option value="development">Restaurant</option>
+                    <option value="marketing">Marketing</option>
+                  </select>
+                  <input
+                    placeholder="Your favorite food"
+                    class="w-full p-4 rounded-full"
+                    type="text"
+                  />
+                  <button
+                    type="button"
+                    title="Start buying"
+                    class="ml-auto py-3 px-6 rounded-full text-center transition bg-gradient-to-b from-yellow-200 to-yellow-300 hover:to-red-300 active:from-yellow-400 focus:from-red-400 md:px-12"
+                  >
+                    <span class="hidden text-yellow-900 font-semibold md:block">
+                      Search
+                    </span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="w-5 mx-auto text-yellow-900 md:hidden"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
                     >
-                      <option value="design">FastFood</option>
-                      <option value="development">Restaurant</option>
-                      <option value="marketing">Marketing</option>
-                    </select>
-                    <input
-                      placeholder="Your favorite food"
-                      class="w-full p-4 rounded-full"
-                      type="text"
-                    />
-                    <button
-                      type="button"
-                      title="Start buying"
-                      class="ml-auto py-3 px-6 rounded-full text-center transition bg-gradient-to-b from-yellow-200 to-yellow-300 hover:to-red-300 active:from-yellow-400 focus:from-red-400 md:px-12"
-                    >
-                      <span
-                        class="hidden text-yellow-900 font-semibold md:block"
-                      >
-                        Search
-                      </span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="w-5 mx-auto text-yellow-900 md:hidden"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </form>
-                <p class="mt-8 text-gray-700 lg:w-10/12">
-                  Sit amet consectetur adipisicing elit.
-                  <a href="#" class="text-yellow-700">connection</a> tenetur
-                  nihil quaerat suscipit, sunt dignissimos.
-                </p>
-              </div>
-              <div class="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
-                <img
-                  src="https://tailus.io/sources/blocks/food-delivery/preview/images/food.webp"
-                  class="relative"
-                  alt="food illustration"
-                  loading="lazy"
-                  width="4500"
-                  height="4500"
-                />
-              </div>
+                      <path
+                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </form>
+              <p class="mt-8 text-gray-700 lg:w-10/12">
+                Sit amet consectetur adipisicing elit.
+                <a href="#" class="text-yellow-700">connection</a> tenetur nihil
+                quaerat suscipit, sunt dignissimos.
+              </p>
+            </div>
+            <div class="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
+              <img
+                src="https://tailus.io/sources/blocks/food-delivery/preview/images/food.webp"
+                class="relative"
+                alt="food illustration"
+                loading="lazy"
+                width="4500"
+                height="4500"
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <footer
-        class="px-3 py-8 bg-white dark:bg-gray-800 text-2 text-gray-500 dark:text-gray-200 transition-colors duration-200"
-      >
-        <div class="flex flex-col">
-          <div class="md:hidden mt-7 mx-auto w-11 h-px rounded-full"></div>
-          <div class="mt-4 md:mt-0 flex flex-col md:flex-row">
-            <nav
-              class="flex-1 flex flex-col items-center justify-center md:items-end md:border-r border-gray-100 md:pr-5"
-            >
-              <a
-                aria-current="page"
-                href="#"
-                class="hover:text-gray-700 dark:hover:text-white"
-              >
-                <br />
-                Dar Krtaket Food Restaurant Company - One Individual Company
-
-                <div class="flex justify-start">
-                  <p class="font-bold">CR#:</p>
-                  <div>101074712</div>
-                </div>
-
-                <div class="flex justify-start">
-                  <p class="font-bold">Address:</p>
-                  <div>
-                    7936 Harun Al Rasheed, Ajazirah Distract, Riyadh, 14251
-                  </div>
-                </div>
-              </a>
-            </nav>
-            <div class="md:hidden mt-4 mx-auto w-11 h-px rounded-full"></div>
-            <div
-              class="mt-4 md:mt-0 flex-1 flex items-center justify-center md:border-r border-gray-100"
-            ></div>
-            <div class="md:hidden mt-4 mx-auto w-11 h-px rounded-full"></div>
-            <div
-              class="mt-8 md:mt-0 flex-1 flex flex-col items-center justify-center md:items-start md:pl-5"
-            ></div>
-          </div>
-        </div>
-      </footer>
     </div>
-  </head>
+
+    <footer
+      class="px-3 py-8 bg-white dark:bg-gray-800 text-2 text-gray-500 dark:text-gray-200 transition-colors duration-200"
+    >
+      <div class="flex flex-col">
+        <div class="md:hidden mt-7 mx-auto w-11 h-px rounded-full"></div>
+        <div class="mt-4 md:mt-0 flex flex-col md:flex-row">
+          <nav
+            class="flex-1 flex flex-col items-center justify-center md:items-end md:border-r border-gray-100 md:pr-5"
+          >
+            <a
+              aria-current="page"
+              href="#"
+              class="hover:text-gray-700 dark:hover:text-white"
+            >
+              <br />
+              Dar Krtaket Food Restaurant Company - One Individual Company
+
+              <div class="flex justify-start">
+                <p class="font-bold">CR#:</p>
+                <div>101074712</div>
+              </div>
+
+              <div class="flex justify-start">
+                <p class="font-bold">Address:</p>
+                <div>7936 Harun Al Rasheed, Ajazirah Distract, Riyadh, 14251</div>
+              </div>
+              
+            </a>
+          </nav>
+          <div class="md:hidden mt-4 mx-auto w-11 h-px rounded-full"></div>
+          <div
+            class="mt-4 md:mt-0 flex-1 flex items-center justify-center md:border-r border-gray-100"
+          >
+           
+            
+          </div>
+          <div class="md:hidden mt-4 mx-auto w-11 h-px rounded-full"></div>
+          <div
+            class="mt-8 md:mt-0 flex-1 flex flex-col items-center justify-center md:items-start md:pl-5"
+          ></div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
